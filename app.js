0fa015b7b55bd3ea918d1dc7dcfeb2e2a3f97523
puya310 <puya310r@gmail.com>
2025-05-23 10:29:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ if (app.get('env') == 'development') {
   app.use(errorHandler());
 }
 
-var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
+//var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
 console.log('token: ' + token);
 
 // deepcode ignore HttpToHttps: <please specify a reason of ignoring this>
